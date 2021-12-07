4e0c490fb84c6d31cc624e59c59cfe7219dd7d30
unknown <markhuang@uitctech.com.tw>
2021-12-07 09:39:33 +0800

    init

---
+++ src/test/java/com/uitc/com/FlutterTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uitc.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlutterTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
